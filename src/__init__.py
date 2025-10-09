686636b61721881a168ef829e6aeba96b70760a1
Gabriele Di Corato <gabriele.dicorato@gmail.com>
2025-10-09 12:26:15 +0200

    Added missin init files

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""
+Root package for the LLM compensation bias research project.
+"""
+
+__version__ = "0.1.0"
---
+++ src/model/__init__.py
@@ -0,0 +1,31 @@
+"""
+Data models for representing persons and their attributes.
+"""
+
+from .person import (
+    Person,
+    Gender,
+    Race,
+    AgeRange,
+    EducationLevel,
+    ExperienceLevel,
+    IndustrySector,
+    EmploymentType,
+    ParentalStatus,
+    DisabilityStatus,
+    CareerGap,
+)
+
+__all__ = [
+    "Person",
+    "Gender",
+    "Race",
+    "AgeRange",
+    "EducationLevel",
+    "ExperienceLevel",
+    "IndustrySector",
+    "EmploymentType",
+    "ParentalStatus",
+    "DisabilityStatus",
+    "CareerGap",
+]
